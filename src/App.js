43c4453fecb2469b2a6d580e0e0fc7436455f21c
Robert Zibert <robert_zibert@hotmail.com>
2016-11-15 01:11:20 -0300

    styling active links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Router, Route, Link, browserHistory} from 'react-router'
+import {Router, Route, Link, browserHistory} from 'react-router'
 
 const Home = () => <div><h1>Home</h1><Links/></div>
 const About = () => <div><h1>About</h1><Links/></div>
@@ -7,9 +7,9 @@ const Contact = () => <div><h1>Contact</h1><Links/></div>
 
 const Links = () =>
   <nav>
-    <Link to='/'>Home</Link>
-    <Link to='/about'>About</Link>
-    <Link to='/contact'>Contact</Link>
+    <Link activeStyle={{color: 'green'}} to='/'>Home</Link>
+    <Link activeStyle={{color: 'green'}} to='/about'>About</Link>
+    <Link activeClassName={'active'} to='/contact'>Contact</Link>
   </nav>
 
 export default class App extends React.Component {
